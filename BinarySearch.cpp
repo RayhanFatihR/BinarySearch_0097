d3fd7ce8ead85a3f91b1f6e663c18de58c4fb1db
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-07 15:26:51 +0700

    Tahap 6

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -55,5 +55,10 @@ void display()
     for (int j = 0; j < nPanjang; j++)
     {
         cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " --> ";
+        }
     }
+    cout << endl;
 }
